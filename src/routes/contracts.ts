5c8dd900fe26008b51a498ee20fc1036104f2a31
Pablo Aravena <paravena74@gmail.com>
2023-12-29 13:36:20 -0300

    contract endpoints done

--- src/routes/contracts.ts
+++ src/routes/contracts.ts
@@ -1,14 +1,33 @@
 import express from 'express';
 import { getProfile } from '../middleware';
+import { Op } from 'sequelize';
+import { sequelize } from '../model';
 
 const router = express.Router();
+const { Contract } = sequelize.models;
+
+router.get('/', getProfile, async (req, res) => {
+  const profile = req.profile;
+  const contracts = await Contract.findAll({
+    where: {
+      status: {
+        [Op.ne]: 'terminated'
+      },
+      ClientId: profile.id
+    }
+  });
+  res.json(contracts);
+});
 
 router.get('/:id', getProfile, async (req, res) => {
-  const { Contract } = req.app.get('models');
-  console.log('Calling endpoint');
   const { id } = req.params;
-  const contract = await Contract.findOne({ where: { id } });
-  console.log('Contract', contract);
+  const profile = req.profile;
+  const contract = await Contract.findOne({
+    where: {
+      id,
+      ClientId: profile.id
+    }
+  });
   if (!contract) return res.status(404).end();
   res.json(contract);
 });
